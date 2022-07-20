45c5dbc034ae54b08e014fe1add9d3704d180b74
bayupw <bayupw@gmail.com>
2022-07-21 05:04:43 +1200

    update resources to equinix_metal_

--- main.tf
+++ main.tf
@@ -10,15 +10,15 @@ resource "equinix_metal_device" "this" {
   metro            = var.metro
   operating_system = var.os
   billing_cycle    = var.billing
-  project_id       = data.metal_project.this.id
+  project_id       = data.equinix_metal_project.this.id
 }
 
 # Set Network to Hybrid
 resource "equinix_metal_device_network_type" "this" {
-  device_id = metal_device.this.id
+  device_id = equinix_metal_device.this.id
   type      = "hybrid"
 
-  depends_on = [metal_device.this]
+  depends_on = [equinix_metal_device.this]
 }
 
 # Create VLANs
@@ -26,11 +26,11 @@ resource "equinix_metal_vlan" "this" {
   for_each = var.vlans
 
   metro       = var.metro
-  project_id  = data.metal_project.this.id
+  project_id  = data.equinix_metal_project.this.id
   vxlan       = each.value.id
   description = each.value.description
 
-  depends_on = [metal_device.this]
+  depends_on = [equinix_metal_device.this]
 }
 
 # Add VLANs to bond0
@@ -41,22 +41,22 @@ resource "equinix_metal_port_vlan_attachment" "this" {
   port_name = "bond0"
   vlan_vnid = each.value.id
 
-  depends_on = [metal_device.this]
+  depends_on = [equinix_metal_device.this]
 }
 
 # Reserve a Public IP block
 resource "equinix_metal_reserved_ip_block" "this" {
-  project_id = data.metal_project.this.id
+  project_id = data.equinix_metal_project.this.id
   type       = "public_ipv4"
   metro      = var.metro
   quantity   = var.num_public_ip
 
-  depends_on = [metal_device.this]
+  depends_on = [equinix_metal_device.this]
 }
 
 # Create a Metal Gateway
 resource "equinix_metal_gateway" "gateway" {
-  project_id        = data.metal_project.this.id
-  vlan_id           = metal_vlan.this["internet_vlan"].id
-  ip_reservation_id = metal_reserved_ip_block.this.id
+  project_id        = data.equinix_metal_project.this.id
+  vlan_id           = equinix_metal_vlan.this["internet_vlan"].id
+  ip_reservation_id = equinix_metal_reserved_ip_block.this.id
 }
