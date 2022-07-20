b825029e7f0d8b608da659a0ca2a6b05ba5d8fa3
bayupw <bayupw@gmail.com>
2022-07-21 05:06:36 +1200

    update resources to equinix_metal_

--- outputs.tf
+++ outputs.tf
@@ -5,12 +5,12 @@
 
 output "metal_gateway_ip" {
   description = "usable_public_ip"
-  value       = cidrhost(metal_reserved_ip_block.this.cidr_notation, 1)
+  value       = cidrhost(equinix_metal_reserved_ip_block.this.cidr_notation, 1)
 }
 
 output "public_ip_block" {
   description = "public_ip_block"
-  value       = metal_reserved_ip_block.this.cidr_notation
+  value       = equinix_metal_reserved_ip_block.this.cidr_notation
 }
 
 output "usable_public_ip" {
@@ -19,6 +19,6 @@ output "usable_public_ip" {
 }
 
 locals {
-  first_public_ip = cidrhost(metal_reserved_ip_block.this.cidr_notation, 2)
-  last_public_ip  = cidrhost(metal_reserved_ip_block.this.cidr_notation, var.num_public_ip - 2)
+  first_public_ip = cidrhost(equinix_metal_reserved_ip_block.this.cidr_notation, 2)
+  last_public_ip  = cidrhost(equinix_metal_reserved_ip_block.this.cidr_notation, var.num_public_ip - 2)
 }
--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     equinix = {
-      source = "equinix/equinix"
-      version = "1.7.0"
+      source  = "equinix/equinix"
+      version = "~> 1.7.0"
     }
   }
 }
